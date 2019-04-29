866d7f0026b6a6f6ed5f1ad91e67cf87101e17f5
Fenil Shah <42182692+FShah1596@users.noreply.github.com>
2019-04-29 15:15:54 -0700

    Add files via upload

---
+++ Angulartest.java
@@ -0,0 +1,40 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Angulartest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Fenil Shah\\Downloads\\chromedriver.exe");
+		ChromeDriver wd = new ChromeDriver();
+		
+		wd.get("http://jsfiddle.net/Fenil96/3451k6o2/9/");
+		wd.manage().window().maximize();
+		wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		
+		//TestCase1
+		wd.findElementById("keyword").sendKeys("ffghghgghghghg");
+		wd.findElementById("search").click();
+		WebElement norecord = wd.findElementByXPath("/html/body/div/div[2]");
+		WebElement record = wd.findElementById("product");
+		assert(norecord.isDisplayed());
+		assert(!record.isDisplayed());
+		wd.findElementById("keyword").clear();
+		wd.findElementById("keyword").sendKeys("India");
+		wd.findElementById("search").click();
+		norecord = wd.findElementByXPath("/html/body/div/div[2]");
+		record = wd.findElementById("product");
+		assert(!norecord.isDisplayed());
+		assert(record.isDisplayed());
+		
+		
+		//Testcase 2
+		wd.findElementById("reset").click();
+		assert(record.isDisplayed());
+		
+	}
+
+}
